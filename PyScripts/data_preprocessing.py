8e4cda9f42bba2dc4db8be4d18f24c57accd8e40
Takeshi Stormer <takeshi.stormer@gmail.com>
2026-01-27 11:33:23 -0800

    Added max/min channels; added individual metric positions between max/min channels.

--- PyScripts/data_preprocessing.py
+++ PyScripts/data_preprocessing.py
@@ -34,13 +34,25 @@ for metric in ['Close PC', 'Open PC', 'High PC', 'Low PC']:
         MODIFIED_DATA=pd.concat([MODIFIED_DATA, MODIFIED_DATA.loc[:, idx[metric, :, :]].shift(lag_period).rename(columns={metric: f"{metric} Lag {lag_period}"}, level=0)], axis=1)
 
 
-# TODO Utilize Volatility to get Z-Score rows
+
+# TODO Utilize Volatility to get Z-Score rows and potentially look into implications of running these feature creation loops while having NA's in the mix due to difference in timezones.
 for metric in ['Close', 'Open', 'High', 'Low']:
     for ema_window in ema_windows: 
         MODIFIED_DATA=pd.concat([MODIFIED_DATA, MODIFIED_DATA.loc[:, idx[metric, :, :]].ewm(span=ema_window, adjust=False).mean().rename(columns={metric: f"{metric} EMA {ema_window}"}, level=0)], axis=1)
     for vol_window in vol_windows:
         MODIFIED_DATA=pd.concat([MODIFIED_DATA, MODIFIED_DATA.loc[:, idx[metric, :, :]].rolling(window=vol_window).std().rename(columns={metric: f"{metric} VOL {vol_window}"}, level=0)], axis=1)
 
+for max_min_window in max_min_windows:
+    MODIFIED_DATA=pd.concat([MODIFIED_DATA, MODIFIED_DATA.loc[:, idx['High', :, :]].rolling(window=max_min_window).max().rename(columns={'High': f"MAX {max_min_window}"}, level=0)], axis=1)
+    MODIFIED_DATA=pd.concat([MODIFIED_DATA, MODIFIED_DATA.loc[:, idx['Low', :, :]].rolling(window=max_min_window).min().rename(columns={'Low': f"MIN {max_min_window}"}, level=0)], axis=1)
+
+for metric in ['Close', 'Open', 'High', 'Low']:
+    for max_min_window in max_min_windows:
+        max_=MODIFIED_DATA.loc[:, idx[f'MAX {max_min_window}', :, :]]
+        min_=MODIFIED_DATA.loc[:, idx[f'MIN {max_min_window}', :, :]]
+        metric_=MODIFIED_DATA.loc[:, idx[metric, :, :]]
+        max_min_channel_pos = (metric_.values - min_.values) / (max_.values - min_.values)
+        MODIFIED_DATA=pd.concat([MODIFIED_DATA, pd.DataFrame(max_min_channel_pos, index=MODIFIED_DATA.index, columns=metric_.columns).rename(columns={metric: f'Channel Position {metric} {max_min_window}'}, level=0)], axis=1)
+
 print(MODIFIED_DATA.columns.levels[0])
-print(MODIFIED_DATA["Close EMA 21"]["Stocks"].iloc[0:100])
-print(MODIFIED_DATA.iloc[100:150])
+print(MODIFIED_DATA["Channel Position Close 21"]["Stocks"].iloc[0:100])
