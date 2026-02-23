661bee507ca3f0be3e4355d10def10e4fbb09d8e
Sabuchan123 <takeshi.stormer@gmail.com>
2026-02-22 21:45:42 -0800

    Updated README.md trivially; updated logistic_regression(classification).py trivially.

--- PyScripts/logistic_regression(classification).py
+++ PyScripts/logistic_regression(classification).py
@@ -3,7 +3,7 @@ from sklearn.linear_model import LogisticRegressionCV, LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, TimeSeriesSplit
 from data_preprocessing_and_cleaning import clean_data
-from model_evaluation import display_feat_importances_logistic, classification_wfv_eval
+from model_evaluation import display_feat_importances_logistic, classification_wfv_eval, classification_accuracy
 from dimension_reduction import RIDGE, apply_PCA, LASSO, step_wise_reg_wfv
 
 from sklearn.pipeline import Pipeline
@@ -30,6 +30,7 @@ if __name__=="__main__":
     # classification_wfv_eval(Log_Reg_model_pipeline_R_L, X_train, y_train)
 
     # ------- LASSO(External)->RIDGE(Internal) APPLICATION -------
+    # X_train, X_test, _, _=train_test_split(X, y_regression, test_size=0.2, random_state=1)
     # X_train=LASSO(X_train, y_train), 
     # X_test=X_test[X_train.columns]
     # tscv=TimeSeriesSplit(n_splits=3)
@@ -44,12 +45,17 @@ if __name__=="__main__":
     # classification_wfv_eval(Log_Reg_model_pipeline_L_R, X_train, y_train)
 
     # ------- PCA APPLICATION -------
+    X_train, X_test, _, _=train_test_split(X, y_regression, test_size=0.2, random_state=1)
     X_train, X_test=apply_PCA(X_train, X_test, n_comp=0.9)
     tscv=TimeSeriesSplit(n_splits=3)
     
     Log_Reg_PCA=LogisticRegression(solver='saga', max_iter=5000, random_state=1, n_jobs=-1)
 
     Log_Reg_PCA.fit(X_train, y_train)
+
+    acc, avg_dir=classification_accuracy(Log_Reg_PCA.predict(X_test), y_test)
+    print("Accuracy (Test):", acc)
+    print("Average Direction (Test):", avg_dir)
 
     display_feat_importances_logistic(Log_Reg_PCA, X_train)
 
