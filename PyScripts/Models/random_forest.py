6b940a854345cdf6c986b7fdfcf5b6b459d3be40
TrangHaNguyen <46385114+TrangHaNguyen@users.noreply.github.com>
2026-03-07 01:32:34 +0000

    Revert "Merge branch 'Sabuchan123:main' into stormer/EDA"
    This reverts commit aea0c4da8c55bf9e5851e4983f01bcccfd811fd0, reversing
    changes made to 17337ff742f669a6444556785aea04a4fe902465.

--- PyScripts/Models/random_forest.py
+++ PyScripts/Models/random_forest.py
@@ -11,40 +11,19 @@ from sklearn.pipeline import Pipeline
 
 from H_reduce import step_wise_reg_wfv
 from H_prep import clean_data
-from H_eval import rolling_window_backtest, get_final_metrics, display_wfv_results
-from H_helpers import append_params_to_dict, log_result, get_cwd
+from H_eval import get_final_metrics_grid, rolling_window_backtest, get_final_metrics
 
 VERBOSE=0
-WINDOW_SIZE=220
-HORIZON=40
-EXPORT=True
-
-cwd=get_cwd("STAT-587-Final-Project")
 
 if __name__=="__main__":
-    WINDOW_SIZE=200
-    HORIZON=40
-    EXPORT=True
-    data_clean_params={
-        "lag_period": [1, 2],
-        "lookback_period": 5,
-        "cluster": True,
-        "sector": True,
-        "corr": True,
-        "corr_level": 3,
-        "testing": False
-    }
-    download_params = {f"clean_data__{k}=": v for k, v in data_clean_params.items()}
-    TEST_SIZE=0.2
-
-    X, y_regression=cast(Any, clean_data(**data_clean_params))
+    X, y_regression=cast(Any, clean_data(lag_period=[1, 2, 3], lookback_period=0, sector=True, corr=True, corr_level=2, corr_threshold=0.95, testing=False)) # You can set cluster=True and sector=True for different variations.
     X_train, X_test, y_train, y_test=train_test_split(X, y_regression, test_size=0.2, random_state=1, shuffle=False)
     def to_binary_class(y):
         return (y>=0).astype(int)
     y_regression=to_binary_class(y_regression)
     y_train=to_binary_class(y_train)
     y_test=to_binary_class(y_test)
-    tscv=TimeSeriesSplit(n_splits=5) # CHANGEABLE (OPTIONAL)
+    tscv=TimeSeriesSplit(n_splits=10) # CHANGEABLE
     
     # ------- BASE APPLICATION -------
     print("\n\n------- Base RF Model -------")
@@ -62,19 +41,13 @@ if __name__=="__main__":
 
     optimized_base_=clone(grid_search_base.best_estimator_)
 
-    wfv_results=rolling_window_backtest(optimized_base_, X, y_regression, verbose=1, window_size=WINDOW_SIZE, horizon=HORIZON)
-    display_wfv_results(wfv_results, X_train, X_test, window_size=WINDOW_SIZE, horizon=HORIZON)
+    rolling_window_backtest(optimized_base_, X, y_regression, verbose=1, window_size=220, horizon=21)
 
     optimized_base_=clone(grid_search_base.best_estimator_)
     optimized_base_.fit(X_train, y_train)
 
-    results=get_final_metrics(optimized_base_, X_train, y_train, X_test, y_test, label="Base RF")
-    if (EXPORT):
-        results=append_params_to_dict(results, clone(optimized_base_))
-        results.update(wfv_results[2])
-        results.update(download_params)
-        log_result(results, cwd / 'output' / 'results', "results.csv")
-    
+    get_final_metrics(optimized_base_, X_train, y_train, X_test, y_test)
+
     input("Press Enter to continue...")
 
     # ------- PCA APPLICATION -------
@@ -95,18 +68,12 @@ if __name__=="__main__":
 
     optimized_PCA_=clone(grid_search_PCA.best_estimator_)
 
-    wfv_results=rolling_window_backtest(optimized_PCA_, X, y_regression, verbose=1, window_size=WINDOW_SIZE, horizon=HORIZON)
-    display_wfv_results(wfv_results, X_train, X_test, window_size=WINDOW_SIZE, horizon=HORIZON)
-
+    rolling_window_backtest(optimized_PCA_, X, y_regression, verbose=1, window_size=220, horizon=21)
+    
     optimized_PCA_=clone(grid_search_PCA.best_estimator_)
     optimized_PCA_.fit(X_train, y_train)
 
-    results=get_final_metrics(optimized_PCA_, X_train, y_train, X_test, y_test, label="PCA RF")
-    if (EXPORT):
-        results=append_params_to_dict(results, clone(optimized_PCA_))
-        results.update(wfv_results[2])
-        results.update(download_params)
-        log_result(results, cwd / 'output' / 'results', "results.csv")
+    get_final_metrics(optimized_PCA_, X_train, y_train, X_test, y_test)
 
     input("Press Enter to continue...")
 
@@ -129,19 +96,13 @@ if __name__=="__main__":
 
     optimized_LASSO_=clone(grid_search_LASSO.best_estimator_)
 
-    wfv_results=rolling_window_backtest(optimized_LASSO_, X, y_regression, verbose=1, window_size=WINDOW_SIZE, horizon=HORIZON)
-    display_wfv_results(wfv_results, X_train, X_test, window_size=WINDOW_SIZE, horizon=HORIZON)
+    rolling_window_backtest(optimized_LASSO_, X, y_regression, verbose=1, window_size=220, horizon=21)
 
     optimized_LASSO_=clone(grid_search_LASSO.best_estimator_)
     optimized_LASSO_.fit(X_train, y_train)
 
-    results=get_final_metrics(optimized_LASSO_, X_train, y_train, X_test, y_test, label="LASSO RF")
-    if (EXPORT):
-        results=append_params_to_dict(results, clone(optimized_LASSO_))
-        results.update(wfv_results[2])
-        results.update(download_params)
-        log_result(results, cwd / 'output' / 'results', "results.csv")
-        
+    get_final_metrics(optimized_LASSO_, X_train, y_train, X_test, y_test)
+
     input("Press Enter to continue...")
 
     # ------- RIDGE APPLICATION -------
@@ -163,19 +124,13 @@ if __name__=="__main__":
 
     optimized_ridge_=clone(grid_search_ridge.best_estimator_)
 
-    wfv_results=rolling_window_backtest(optimized_ridge_, X, y_regression, verbose=1, window_size=WINDOW_SIZE, horizon=HORIZON)
-    display_wfv_results(wfv_results, X_train, X_test, window_size=WINDOW_SIZE, horizon=HORIZON)
+    rolling_window_backtest(optimized_ridge_, X, y_regression, verbose=1, window_size=220, horizon=21)
 
     optimized_ridge_=clone(grid_search_ridge.best_estimator_)
     optimized_ridge_.fit(X_train, y_train)
 
-    results=get_final_metrics(optimized_ridge_, X_train, y_train, X_test, y_test, label="Ridge RF")
-    if (EXPORT):
-        results=append_params_to_dict(results, clone(optimized_ridge_))
-        results.update(wfv_results[2])
-        results.update(download_params)
-        log_result(results, cwd / 'output' / 'results', "results.csv")
-        
+    get_final_metrics(optimized_ridge_, X_train, y_train, X_test, y_test)
+
     input("Press Enter to continue...")
 
     # ------- STEP-WISE REGRESSION APPLICATION -------
@@ -214,16 +169,10 @@ if __name__=="__main__":
 
     RFClassifier_red_sw_wfv.fit(X_train_final, y_train)
 
-    wfv_results=rolling_window_backtest(RFClassifier_red_sw_wfv, X[X_train_final.columns], y_regression, verbose=1, window_size=WINDOW_SIZE, horizon=21)
-    display_wfv_results(wfv_results, X_train, X_test, window_size=WINDOW_SIZE, horizon=HORIZON)
+    rolling_window_backtest(RFClassifier_red_sw_wfv, X[X_train_final.columns], y_regression, verbose=1, window_size=220, horizon=21)
 
     RFClassifier_red_sw_wfv.fit(X_train_final, y_train)
 
-    results=get_final_metrics(RFClassifier_red_sw_wfv, X_train_final, y_train, X_test_final, y_test, label="Stepwise RF")
-    if (EXPORT):
-        results=append_params_to_dict(results, clone(RFClassifier_red_sw_wfv))
-        results.update(wfv_results[2])
-        results.update(download_params)
-        log_result(results, cwd / 'output' / 'results', "results.csv")
-        
+    get_final_metrics(RFClassifier_red_sw_wfv, X_train_final, y_train, X_test_final, y_test)
+
     input("Press Enter to Finish...")
